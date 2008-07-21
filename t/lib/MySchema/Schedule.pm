f54017f94f103cad4ab5630ac032e59cb9727d4a
fireartist <fireartist@gmail.com>
2008-07-21 09:52:12 +0000

    adding new schema file that should have been included in r1081
    git-svn-id: file:///home/cafranks/github/googlecode/trunk/HTML-FormFu-Model-DBIC@1082 3452f2c1-2087-41cb-87be-0eefa6d42935

---
+++ t/lib/MySchema/Schedule.pm
@@ -0,0 +1,23 @@
+package MySchema::Schedule;
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/ InflateColumn::DateTime Core /);
+
+__PACKAGE__->table("schedule");
+
+__PACKAGE__->add_columns(
+    id     => { data_type => "INTEGER", is_nullable => 0 },
+    master => { data_type => "INTEGER", is_nullable => 0 },
+    date   => { data_type => "DATETIME", is_nullable => 0 },
+    note   => { data_type => "TEXT", is_nullable => 0 },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to( master => 'MySchema::Master' );
+
+1;
+
